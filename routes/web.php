8a698f372a4e3c46c9fd81e42faa9d5432f6465a
Felipe Diniz <felipedinizdossantos0@gmail.com>
2026-08-15 19:17:17 -0300

    main

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,6 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [MainController::class, 'index'])->name('index');
+Route::get('/', function () {
+    echo 'Hello, World!';
+});
